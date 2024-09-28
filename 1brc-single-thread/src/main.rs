1ee4019f6b3744ab36b48167d49789024f2f91fb
Youn Mélois <youn.melois@clever-cloud.com>
2024-09-29 00:39:49 +0200

    rename map to Records

--- 1brc-single-thread/src/main.rs
+++ 1brc-single-thread/src/main.rs
@@ -6,14 +6,14 @@ use std::fs::File;
 
 use crate::iter::line_chunks::IterLineChunks;
 use crate::iter::raw_records::IterRawRecords;
-use crate::record::map::RecordMap;
+use crate::record::records::Records;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sample_file_path = std::env::args().nth(1).expect("No file path provided");
 
     let file = File::open(&sample_file_path)?;
 
-    let mut records = RecordMap::new();
+    let mut records = Records::new();
 
     for line_chunk in file.iter_line_chunks() {
         for record in line_chunk.iter_raw_records() {
