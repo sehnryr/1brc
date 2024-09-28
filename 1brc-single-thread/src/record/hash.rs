a9d39fdeff25477756f99534f6b38e905ab69c0b
Youn Mélois <youn.melois@clever-cloud.com>
2024-09-28 23:00:32 +0200

    simplify hash function

--- 1brc-single-thread/src/record/hash.rs
+++ 1brc-single-thread/src/record/hash.rs
@@ -6,18 +6,15 @@ impl Hash for &[u8] {
     #[inline(always)]
     fn hash(&self) -> u16 {
         let mut hash: u16 = self.len() as u16;
-        let mut sum: u16 = 0x517c;
 
-        for &byte in self.iter().take(4) {
+        for &byte in self.iter().take(5) {
             hash = hash.rotate_left(3) ^ byte as u16;
-            sum = sum.wrapping_add(byte as u16);
         }
 
-        for &byte in self.iter().rev().take(4) {
+        for &byte in self.iter().rev().take(5) {
             hash = hash.rotate_left(3) ^ byte as u16;
-            sum = sum.wrapping_add(byte as u16);
         }
 
-        hash ^ sum
+        hash
     }
 }
