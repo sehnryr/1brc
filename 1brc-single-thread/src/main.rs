8c73a56e565f68dcf927b84cb812df822de75030
Youn Mélois <youn.melois@clever-cloud.com>
2024-09-29 20:16:59 +0200

    create chunk builder to prepare for multi-thread support

--- 1brc-single-thread/src/main.rs
+++ 1brc-single-thread/src/main.rs
@@ -1,24 +1,26 @@
+mod chunk_builder;
 mod hash;
 mod iter;
 mod record;
 mod util;
 
-use std::fs::File;
-
-use crate::iter::{IterLineChunks, IterRawRecords};
+use crate::chunk_builder::ChunkBuilder;
+use crate::iter::IterRawRecords;
 use crate::record::Records;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sample_file_path = std::env::args().nth(1).expect("No file path provided");
 
-    let file = File::open(&sample_file_path)?;
+    let chunks = ChunkBuilder::new(sample_file_path);
 
     let mut records = Records::new();
 
-    for line_chunk in file.iter_line_chunks() {
-        for record in line_chunk.iter_raw_records() {
+    let mut i = 0;
+    while let Ok(chunk) = chunks.get_chunk(i, 1_000_000) {
+        for record in chunk.iter_raw_records() {
             records.add(record);
         }
+        i += 1;
     }
 
     for record in records.into_iter() {
---
+++ 1brc-single-thread/src/util/mod.rs
@@ -0,0 +1,5 @@
+mod seek_helper;
+mod temperature;
+
+pub use seek_helper::SeekByteHelper;
+pub use temperature::parse_temperature;
