d0e89b750fd8c2cd36205ff0a43ab73e1844cea6
sehnryr <youn@melois.dev>
2024-09-30 04:29:57 +0200

    use slice pattern for hash

--- src/hash.rs
+++ src/hash.rs
@@ -1,3 +1,14 @@
+trait HashStep {
+    fn step_hash_1(&mut self, byte: u8);
+}
+
+impl HashStep for u16 {
+    #[inline(always)]
+    fn step_hash_1(&mut self, byte: u8) {
+        *self = self.rotate_left(3) ^ byte as u16;
+    }
+}
+
 pub trait Hash {
     fn hash(&self) -> u16;
 }
@@ -7,12 +18,48 @@ impl Hash for &[u8] {
     fn hash(&self) -> u16 {
         let mut hash: u16 = self.len() as u16;
 
-        for &byte in self.iter().take(5) {
-            hash = hash.rotate_left(3) ^ byte as u16;
+        match self {
+            [d0, d1, d2, d3, d4, ..] => {
+                hash.step_hash_1(*d0);
+                hash.step_hash_1(*d1);
+                hash.step_hash_1(*d2);
+                hash.step_hash_1(*d3);
+                hash.step_hash_1(*d4);
+            }
+            [d0, d1, d2, d3] => {
+                hash.step_hash_1(*d0);
+                hash.step_hash_1(*d1);
+                hash.step_hash_1(*d2);
+                hash.step_hash_1(*d3);
+            }
+            [d0, d1, d2] => {
+                hash.step_hash_1(*d0);
+                hash.step_hash_1(*d1);
+                hash.step_hash_1(*d2);
+            }
+            _ => {}
         }
 
-        for &byte in self.iter().rev().take(5) {
-            hash = hash.rotate_left(3) ^ byte as u16;
+        match self {
+            [.., d4, d3, d2, d1, d0] => {
+                hash.step_hash_1(*d4);
+                hash.step_hash_1(*d3);
+                hash.step_hash_1(*d2);
+                hash.step_hash_1(*d1);
+                hash.step_hash_1(*d0);
+            }
+            [.., d3, d2, d1, d0] => {
+                hash.step_hash_1(*d3);
+                hash.step_hash_1(*d2);
+                hash.step_hash_1(*d1);
+                hash.step_hash_1(*d0);
+            }
+            [.., d2, d1, d0] => {
+                hash.step_hash_1(*d2);
+                hash.step_hash_1(*d1);
+                hash.step_hash_1(*d0);
+            }
+            _ => {}
         }
 
         hash
