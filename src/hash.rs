aefb86fa9cc3dff672b0d6c4b4096612e0af50b3
sehnryr <youn@melois.dev>
2024-10-05 23:47:50 +0200

    simplify hash even more

--- src/hash.rs
+++ src/hash.rs
@@ -1,14 +1,3 @@
-trait HashStep {
-    fn rotate_xor(&self, byte: u8, shift: u32) -> u16;
-}
-
-impl HashStep for u16 {
-    #[inline(always)]
-    fn rotate_xor(&self, byte: u8, shift: u32) -> u16 {
-        self.rotate_left(shift) ^ byte as u16
-    }
-}
-
 pub trait Hash {
     fn hash(&self) -> u16;
 }
@@ -17,16 +6,10 @@ impl Hash for &[u8] {
     #[inline(always)]
     fn hash(&self) -> u16 {
         let len = self.len();
-        let mut hash: u16 = len as u16;
-
-        hash = hash.rotate_xor(self[0], 3);
-        hash = hash.rotate_xor(self[1], 7);
-        hash = hash.rotate_xor(self[2], 5);
-
-        hash = hash.rotate_xor(self[len - 1], 3);
-        hash = hash.rotate_xor(self[len - 2], 5);
-        hash = hash.rotate_xor(self[len - 3], 7);
-
+        let mut hash = len as u16 ^ self[0] as u16;
+        hash = hash.rotate_left(4) ^ self[1] as u16;
+        hash = hash.rotate_left(7) ^ self[2] as u16;
+        hash = hash.rotate_left(11) ^ self[len - 3] as u16;
         hash
     }
 }
