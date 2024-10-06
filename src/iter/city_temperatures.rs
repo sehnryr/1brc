3e9042db670203198cbffd3fd2b0a5887c13e116
sehnryr <youn@melois.dev>
2024-10-06 19:00:55 +0200

    refactor using macro_rules for char to int conversion

--- src/iter/city_temperatures.rs
+++ src/iter/city_temperatures.rs
@@ -1,33 +1,36 @@
 use crate::util::FindByte;
 
-#[inline(always)]
-pub fn temperature_from_digits(d2: &u8, d1: &u8, d0: &u8) -> i32 {
-    let d2 = *d2 as u16;
-    let d1 = *d1 as u16;
-    let d0 = *d0 as u16;
+macro_rules! ctoi {
+    ($value:expr) => {
+        ($value - b'0') as i32
+    };
+}
 
-    let temperature = d2 * 100 + d1 * 10 + d0;
-    let normalized = temperature - 5328; // b'0' * 100 + b'0' * 10 + b'0'
-
-    normalized as i32
+macro_rules! atoi {
+    ($d2:expr, $d1:expr, $d0:expr) => {
+        ctoi!($d2) * 100 + ctoi!($d1) * 10 + ctoi!($d0)
+    };
+    ($d1:expr, $d0:expr) => {
+        ctoi!($d1) * 10 + ctoi!($d0)
+    };
 }
 
 #[inline(always)]
 fn parse_city_temperature(chunk: &[u8]) -> (&[u8], i32, usize) {
     // Since city names have at least 3 characters, we can skip the first 3 bytes.
-    let city_len = 3 + &chunk[3..].find_byte_index(b';');
-
+    let city_len = 3 + chunk[3..].find_byte_index(b';');
     let city = &chunk[..city_len];
 
     let (temp, temp_len) = match &chunk[city_len + 1..] {
-        [b'-', d2, d1, b'.', d0, ..] => (-temperature_from_digits(d2, d1, d0), 5),
-        [b'-', d1, b'.', d0, ..] => (-temperature_from_digits(&b'0', d1, d0), 4),
-        [d2, d1, b'.', d0, ..] => (temperature_from_digits(d2, d1, d0), 4),
-        [d1, b'.', d0, ..] => (temperature_from_digits(&b'0', d1, d0), 3),
+        [b'-', d2, d1, b'.', d0, ..] => (-atoi!(d2, d1, d0), 5),
+        [b'-', d1, b'.', d0, ..] => (-atoi!(d1, d0), 4),
+        [d2, d1, b'.', d0, ..] => (atoi!(d2, d1, d0), 4),
+        [d1, b'.', d0, ..] => (atoi!(d1, d0), 3),
         _ => unreachable!(),
     };
 
-    (city, temp, city_len + temp_len + 2)
+    let read_len = city_len + temp_len + 2;
+    (city, temp, read_len)
 }
 
 pub struct ToCityTemperatures<'a> {
