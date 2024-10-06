8e68605fb5296de298e424eb4f238253d3ab9944
sehnryr <youn@melois.dev>
2024-10-06 19:06:21 +0200

    remove awkward parse function which returns many thing and wasn't named right

--- src/iter/city_temperatures.rs
+++ src/iter/city_temperatures.rs
@@ -15,24 +15,6 @@ macro_rules! atoi {
     };
 }
 
-#[inline(always)]
-fn parse_city_temperature(chunk: &[u8]) -> (&[u8], i32, usize) {
-    // Since city names have at least 3 characters, we can skip the first 3 bytes.
-    let city_len = 3 + chunk[3..].find_byte_index(b';');
-    let city = &chunk[..city_len];
-
-    let (temp, temp_len) = match &chunk[city_len + 1..] {
-        [b'-', d2, d1, b'.', d0, ..] => (-atoi!(d2, d1, d0), 5),
-        [b'-', d1, b'.', d0, ..] => (-atoi!(d1, d0), 4),
-        [d2, d1, b'.', d0, ..] => (atoi!(d2, d1, d0), 4),
-        [d1, b'.', d0, ..] => (atoi!(d1, d0), 3),
-        _ => unreachable!(),
-    };
-
-    let read_len = city_len + temp_len + 2;
-    (city, temp, read_len)
-}
-
 pub struct ToCityTemperatures<'a> {
     chunk: &'a [u8],
     position: usize,
@@ -48,9 +30,22 @@ impl<'a> Iterator for ToCityTemperatures<'a> {
         }
 
         let chunk = &self.chunk[self.position..];
-        let (city, temp, len) = parse_city_temperature(chunk);
 
-        self.position += len;
+        // Since city names have at least 3 characters, we can skip the first 3 bytes.
+        let city_len = 3 + chunk[3..].find_byte_index(b';');
+        let city = &chunk[..city_len];
+
+        // We don't need to check for the length of the temperature by finding the next line break.
+        // We can assume that the temperature is always 2 or 3 digits long.
+        let (temp, temp_len) = match &chunk[city_len + 1..] {
+            [b'-', d2, d1, b'.', d0, ..] => (-atoi!(d2, d1, d0), 5),
+            [b'-', d1, b'.', d0, ..] => (-atoi!(d1, d0), 4),
+            [d2, d1, b'.', d0, ..] => (atoi!(d2, d1, d0), 4),
+            [d1, b'.', d0, ..] => (atoi!(d1, d0), 3),
+            _ => unreachable!(),
+        };
+
+        self.position += city_len + temp_len + 2;
 
         Some((city, temp))
     }
